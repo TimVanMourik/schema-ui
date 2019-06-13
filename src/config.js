cff9d1a3bb10ccffe494d05d4e5ce29236f55898
Remi Gau <remi_gau@hotmail.com>
2019-06-13 18:07:38 +0200

    config.js in sc point to COBIDAS branch on remi's repo

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/ReproNim/schema-standardization/master/activity-sets/voice-pilot/voice_pilot_schema.jsonld',
-  // githubSrc: 'https://raw.githubusercontent.com/ReproNim/schema-standardization/master/activity-sets/ema-hbn/ema-hbn_schema.jsonld',
+  githubSrc: 'https://raw.githubusercontent.com/Remi-Gau/schema-standardization/COBIDAS/activity-sets/cobidas/cobidas_schema.jsonld',
+  // githubSrc: 'https://raw.githubusercontent.com/Remi-Gau/schema-standardization/master/activity-sets/cognitive-tasks/cognitive-tasks_context.jsonld',
   // githubSrc: 'https://raw.githubusercontent.com/sanuann/schema-standardization/branching_server_side_test/activity-sets/voice-pilot/voice_pilot_schema.jsonld',
-  contentSrc: 'https://raw.githubusercontent.com/ReproNim/schema-standardization/master/activity-sets/voice-pilot/README.md'
+  contentSrc: 'https://raw.githubusercontent.com/Remi-Gau/schema-standardization/COBIDAS/activity-sets/cobidas/README.md'
 };
